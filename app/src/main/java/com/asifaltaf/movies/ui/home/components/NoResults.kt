40d4e6a1aad0c18326b86003aab84b153cf85737
Asif Altaf <asifaltaf13@gmail.com>
2023-05-03 18:04:52 +0200

    ux improvement: rotation animation added to no results image

--- app/src/main/java/com/asifaltaf/movies/ui/home/components/NoResults.kt
+++ app/src/main/java/com/asifaltaf/movies/ui/home/components/NoResults.kt
@@ -1,5 +1,10 @@
 package com.asifaltaf.movies.ui.home.components
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,10 +14,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.asifaltaf.movies.R
 import com.asifaltaf.movies.ui.theme.Phosphate
@@ -27,11 +35,26 @@ fun NoResults() {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            val rotateAnimationDuration = 2*60*1000
+            val infiniteTransition = rememberInfiniteTransition()
+            val rotateAnimationDegrees by infiniteTransition.animateFloat(
+                initialValue = 0f,
+                targetValue = 360f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(
+                        durationMillis = rotateAnimationDuration,
+                        easing = LinearEasing
+                    )
+                )
+            )
+
             Image(
+                modifier = Modifier
+                    .rotate(degrees = rotateAnimationDegrees),
                 painter = painterResource(id = R.drawable.no_results),
                 contentDescription = stringResource(R.string.no_results)
             )
-            Spacer(modifier = Modifier.padding(8.dp))
+            Spacer(modifier = Modifier.padding(16.dp))
             Text(
                 text = "Nothing to show",
                 fontFamily = Phosphate,
@@ -39,4 +62,10 @@ fun NoResults() {
             )
         }
     }
+}
+
+@Preview
+@Composable
+fun NoResultPreview() {
+    NoResults()
 }
